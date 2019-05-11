8423a15e4ec1507366e0932696a081b906d66cc6
liyuxing <yuxing.li@ims.jsfund.cn>
2019-05-11 23:54:08 +0800

    增加dict模型

--- dicts/models.py
+++ dicts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class CM_DICT(models.Model):
+    name = models.CharField(max_length=100) # 字典名，EN
+    desc = models.CharField(max_length=100) # 字典描述
+    dataType = models.IntegerField()
