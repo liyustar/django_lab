a1f1ec3a68105fa76b76d007d0561f7937fd1e55
liyuxing <yuxing.li@ims.jsfund.cn>
2019-05-12 10:52:33 +0800

    delete unuse code

--- dicts/models.py
+++ dicts/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class CmDict(models.Model):
-    name = models.CharField(max_length=100) # 字典名，EN
+    name = models.CharField(primary_key=True, max_length=100) # 字典名，EN
     desc = models.CharField(max_length=100) # 字典描述
     dataType = models.IntegerField()
